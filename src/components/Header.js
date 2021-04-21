528a6b35e52ade42be4de808ff0e96bff6d09c0a
avi9360 <savinash406@gmail.com>
2021-04-21 20:03:12 +0530

    added team

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ import HomeIcon from "@material-ui/icons/Home";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-
+import DirectionsBusIcon from "@material-ui/icons/DirectionsBus";
+import EmailIcon from "@material-ui/icons/Email";
 export default function Header() {
   const [error, setError] = useState("");
 
@@ -24,32 +25,35 @@ export default function Header() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <HomeIcon />
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <Link
+                  className="nav-link active pl-0 pr-1 ml-0"
+                  aria-current="page"
+                  to="/"
+                >
+                  <HomeIcon />
                   Home
                 </Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="bus-route">
-                  Bus Route
+                  <DirectionsBusIcon /> Bus Route
                 </Link>
               </li>
             </ul>
           </div>
+
           <div className="d-flex flex-row-reverse">
             <Button className="btn btn-dark btn-sm" onClick={handleLogout}>
               Log Out
             </Button>
-            <Link to="/update-profile" className="btn btn-primary btn-sm mr-3">
+            <Link to="/update-profile" className="link-primary mr-3">
               Update Profile
             </Link>
-            <div className="ml-5 mr-3">
-              <strong>email: </strong>
-              {currentUser.email}
-            </div>
+            <div className="ml-2 mr-3">{currentUser.email}</div>
+            <EmailIcon />
             {error && <Alert variant="danger">{error}</Alert>}
           </div>
         </div>
