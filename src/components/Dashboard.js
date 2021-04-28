f18cc38ffc9d1bc12f11308ef540b720e4df2db3
avi9360 <savinash406@gmail.com>
2021-04-28 20:12:17 +0530

    pay.json added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import BusRoute from "./BusRoute";
 import Weather from "./Weather";
-import { Card, CardDeck, Container } from "react-bootstrap";
+import { Card, CardDeck, Alert, Container, Button } from "react-bootstrap";
 
 export default function Dashboard() {
+  // eslint-disable-next-line
+  const [type, setType] = useState("danger");
+  // eslint-disable-next-line
+  const [message, setMessage] = useState(
+    "Oops! You have not paid the fees yet!"
+  );
   return (
     <>
       <Header />
+      <AlertDismissibleExample type={type} message={message} />
       <h2 className="text-center mt-4 mb-4">
         <strong>Profile</strong>
       </h2>
@@ -61,5 +68,29 @@ export default function Dashboard() {
         </div> */}
       </>
     </>
+  );
+}
+
+function AlertDismissibleExample({ type, message }) {
+  const [show, setShow] = useState(true);
+
+  if (show) {
+    return (
+      <Alert
+        variant={type}
+        onClose={() => setShow(false)}
+        dismissible
+        className="d-flex align-items-center justify-content-center"
+      >
+        <Alert.Heading>{message}</Alert.Heading>
+      </Alert>
+    );
+  }
+  return (
+    <div className="d-flex align-items-center justify-content-center pt-2">
+      <Button onClick={() => setShow(true)} className="btn btn-primary btn-sm">
+        Show Alert
+      </Button>
+    </div>
   );
 }
--- src/components/Fees.js
+++ src/components/Fees.js
@@ -1,12 +1,16 @@
 import React from "react";
+// import firebase from "firebase/app";
+import "firebase/firestore";
 import Header from "./Header";
 import Footer from "./Footer";
+
+// const firestore = firebase.firestore();
 
 export default function Fees() {
   return (
     <>
       <Header />
-      <div>fees</div>
+      {/* <div>fees </div> */}
       <Footer />
     </>
   );
