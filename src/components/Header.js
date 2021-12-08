3319a0e694ae79d6d60b9fc6b6adc9b7cb7fb3c8
avi9360 <savinash406@gmail.com>
2021-12-08 06:34:35 +0530

    Final result 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,7 +39,13 @@ export default function Header() {
             ) : (
               <></>
             )}
-
+            {auth.currentUser.uid === "QFpi0G7XF5VB1sIWnHWTBzTO9rR2" ? (
+              <Nav.Link as={Link} to="https://cmrqr.vercel.app/">
+                SCAN
+              </Nav.Link>
+            ) : (
+              <></>
+            )}
             <NavDropdown title="MORE" id="collasible-nav-dropdown">
               <NavDropdown.Item as={Link} to="bus-route">
                 Bus Route
